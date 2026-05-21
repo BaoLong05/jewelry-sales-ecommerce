b45564ebe16fd9f6a0619f3d4f621657dbc66ac9
Trần Bảo Long <long.dp2005@gmail.com>
2026-05-21 14:33:54 +0700

    fix cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,15 +1,26 @@
 <?php
 
 return [
-    'paths'                    => ['api/*'],
-    'allowed_methods'          => ['*'],
-    'allowed_origins'          => [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
         'http://192.168.33.13:5173',
         'http://localhost:5173',
+        'https://jewelry-sales-ecommerce.pages.dev',
+        'https://4b2a2491.jewelry-sales-ecommerce.pages.dev',
     ],
-    'allowed_origins_patterns' => [],
-    'allowed_headers'          => ['*'],
-    'exposed_headers'          => [],
-    'max_age'                  => 0,
-    'supports_credentials'     => true,
+
+    'allowed_origins_patterns' => [
+        '/^https:\/\/.*\.jewelry-sales-ecommerce\.pages\.dev$/',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
 ];
